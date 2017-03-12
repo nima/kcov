e947f0983a8d894ca54a64a3d01fb9caa69a0c2d
Simon Kagstrom <simon.kagstrom@gmail.com>
2017-03-12 13:41:25 +0100

    WIP: Binary rewriter library

---
+++ src/engines/dyninst-binary-lib.cc
@@ -0,0 +1,86 @@
+#include <sys/types.h>
+#include <time.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <utils.hh>
+#include <swap-endian.hh>
+
+const uint32_t magic = 0x4d455247; // "MERG"
+const uint32_t version = 1;
+
+struct Instance
+{
+	uint32_t *bits;
+	size_t bitVectorSize;
+	uint64_t id;
+};
+
+static Instance g_instance;
+
+static void write_report(unsigned int idx)
+{
+	(void)mkdir("/tmp/kcov-data", 0755);
+
+	std::string out = fmt("/tmp/kcov-data/%016llx.%u", g_instance.id, idx);
+	FILE *fp = fopen(out.c_str(), "w");
+
+	// What to do?
+	if (!fp)
+		return;
+
+	fwrite(&magic, sizeof(magic), 1, fp);
+	fwrite(&version, sizeof(version), 1, fp);
+	fwrite(g_instance.bits, sizeof(uint32_t), g_instance.bitVectorSize, fp);
+
+	fclose(fp);
+}
+
+void kcov_dyninst_binary_init(uint64_t id, size_t vectorSize)
+{
+	g_instance.bits = (uint32_t *)malloc(vectorSize * sizeof(uint32_t));
+	g_instance.bitVectorSize = vectorSize;
+	g_instance.id = id;
+}
+
+void kcov_dyninst_binary_report_address(unsigned int bitIdx)
+{
+	unsigned int wordIdx = bitIdx / 32;
+	unsigned int offset = bitIdx % 32;
+
+	if (wordIdx >= g_instance.bitVectorSize)
+	{
+		fprintf(stderr, "kcov: INTERNAL ERROR: Index out of bounds (%u vs %zu)\n",
+				wordIdx, g_instance.bitVectorSize);
+		return;
+	}
+
+	// Update the bit atomically
+	uint32_t *p = &g_instance.bits[wordIdx];
+
+	// Already hit?
+	if (*p & (1 << offset))
+		return;
+
+	uint32_t val, newVal;
+	do
+	{
+		val = *p;
+		newVal = val | (1 << offset);
+
+	} while (!__sync_bool_compare_and_swap(p, val, newVal));
+
+
+	// Write out the report
+	static time_t last_time;
+
+	time_t now = time(NULL);
+	if (now - last_time >= 2)
+	{
+		write_report(bitIdx);
+		last_time = now;
+	}
+}
